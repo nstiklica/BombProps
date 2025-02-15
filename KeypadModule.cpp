d517071ec56968ca4c3225a6bb151c6fea00a4dd
nestiklica <n.stiklica@teamasaservice.com>
2025-02-15 20:04:36 +0100

    Added KeypadModule class

---
+++ KeypadModule.cpp
@@ -0,0 +1,16 @@
+#include "KeypadModule.h"
+
+KeypadModule::KeypadModule(byte* rowPins, byte* colPins) {
+    this->rowPins = rowPins;
+    this->colPins = colPins;
+    keypad = new Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+}
+
+void KeypadModule::begin(){
+    //Some initialization logic if needed
+}
+
+char KeypadModule::getKey(){
+    return keypad->getKey();
+}
+
---
+++ KeypadModule.h
@@ -0,0 +1,28 @@
+#ifndef KEYPADMODULE_H
+#define KEYPADMODULE_H
+
+#include <Arduino.h>
+#include <Keypad.h>
+
+class KeypadModule {
+public:
+    KeypadModule(byte* rowPins, byte* colPins);
+    void begin();
+    char getKey();
+
+private:
+    static const byte ROWS = 4;
+    static const byte COLS = 4;
+    char keys[ROWS][COLS] = {
+        {'1', '2', '3', 'A'},
+        {'4', '5', '6', 'B'},
+        {'7', '8', '9', 'C'},
+        {'*', '0', '#', 'D'}
+    };
+    
+    byte* rowPins;
+    byte* colPins;
+    Keypad* keypad;
+};
+
+#endif
