81dbfb2d28205a238c632e8fbddefe8e04424dbc
nestiklica <n.stiklica@teamasaservice.com>
2025-02-19 16:15:27 +0100

    Added DisarmView

---
+++ DisarmView.h
@@ -0,0 +1,22 @@
+#ifndef DISARMVIEW_H
+#define DISARMVIEW_H
+
+#include "BaseView.h"
+#include "BasicBombGame.h"
+
+class DisarmView : public BaseView {
+public:
+    DisarmView(void (*stateChangeCallback)(BombState));
+
+    void render() override;
+    void refresh() override;
+    void handleInput(char key) override;
+    void drawInfoSection(const char* text, uint16_t textColor, uint16_t bgColor) override;
+private:
+    char enteredCode[7];
+    int codeIndex;
+
+    void updateCodeDisplay();
+};
+
+#endif
