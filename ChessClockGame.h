1f7191d1aaa8344f587da12907c3581bd73eabe8
nestiklica <n.stiklica@teamasaservice.com>
2025-03-07 10:45:26 +0100

    Added initial implementation of ChessClockGame

---
+++ ChessClockGame.h
@@ -0,0 +1,14 @@
+#ifndef CHESSCLOCKGAME_H
+#define CHESSCLOCKGAME_H
+
+#include "BombGame.h"
+
+class ChessClockGame : public BombGame {
+    public:
+        ChessClockGame();
+
+        void begin() override;
+        void update() override;
+};
+
+#endif
