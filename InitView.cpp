ebad7d597f5d9c2da86b542725a187404c1f12ed
nstiklica <nstiklica@live.com>
2025-04-12 19:28:05 +0200

    Added new timer timw

--- InitView.cpp
+++ InitView.cpp
@@ -6,14 +6,14 @@ InitView::InitView(void (*stateChangeCallback)(BasicBombGameStates))
 void InitView::render() {
   screen->fillScreen(ST77XX_BLACK);
   drawInfoSection("Select Mode", ST77XX_WHITE, ST77XX_BLUE);
-  drawMainSection("(1) 5 mins\n----------\n(2) 30 mins", ST77XX_WHITE, ST77XX_BLACK);
-  drawControlSection("Press (1) or (2)", ST77XX_WHITE, ST77XX_BLACK);
+  drawMainSection("(1) 5 mins\n----------\n(2) 10 mins\n----------\n(3) 30 mins", ST77XX_WHITE, ST77XX_BLACK);
+  drawControlSection("Press number", ST77XX_WHITE, ST77XX_BLUE);
 }
 
 void InitView::drawMainSection(const char* content, uint16_t textColor, uint16_t bgColor) {
   screen->setTextSize(2);
   screen->setTextColor(textColor);
-  screen->setCursor(0, 45);
+  screen->setCursor(0, 33);
   screen->print(content);
 }
 
@@ -23,6 +23,10 @@ void InitView::handleInput(char key) {
     stateChangeCallback(BasicBombGameStates::ARMING);
   }
   else if (key == '2') {
+    BasicBombGame::setTime(0 , 10);
+    stateChangeCallback(BasicBombGameStates::ARMING);
+  }
+  else if (key == '3') {
     BasicBombGame::setTime(0 , 30);
     stateChangeCallback(BasicBombGameStates::ARMING);
   }
