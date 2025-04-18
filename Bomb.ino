dec6db1fc392fa7cd2ada42e5ee97b733972bcc5
nstiklica <nstiklica@live.com>
2025-04-18 13:21:30 +0200

    Changed keypad pins

--- Bomb.ino
+++ Bomb.ino
@@ -2,14 +2,14 @@
 #include "BasicBombGame.h"
 #include "InitializeGameView.h"
 
-byte rowPins2[4] = {9, 8, 7, 6};
+byte rowPins2[4] = {9, 6, A3, A4};
 byte colPins2[4] = {5, 4, 3, 2};
 KeypadModule keypad(rowPins2, colPins2);
 
 BombGame* game;
 
 void setup() {
-
+ 
   pinMode(A5, OUTPUT);
   digitalWrite(A5, HIGH);
 
